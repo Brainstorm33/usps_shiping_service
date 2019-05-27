a7f5aa068e24830e511dcb4cc4880998890e70bf
Karlen92 <mkkarlen92@gmail.com>
2019-05-27 17:18:52 +0400

    Added logger

--- controllers/rate.js
+++ controllers/rate.js
@@ -99,7 +99,7 @@ async function rateShipping(ctx) {
 
     _.set(ctx, 'body', shipment);
   } catch (err) {
-    console.log(err);
+    console.log(JSON.stringify(err));
     const message = err.Description;
     throw new Error(message);
   }
