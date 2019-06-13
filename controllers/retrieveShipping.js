a572835eed8fba33ce7b9fc7f15298e267c624d8
Karlen92 <mkkarlen92@gmail.com>
2019-06-13 17:42:36 +0400

    Code refactoring

--- controllers/retrieveShipping.js
+++ controllers/retrieveShipping.js
@@ -8,38 +8,19 @@ async function retrieveShipping(ctx) {
   const api = new Easypost(USPS_KEY);
 
   const data = _.get(ctx, 'request.body');
-  const { shipmentId } = data;
+  const { shipmentId, shippingType } = data;
 
   try {
     const shipment = await api.Shipment.retrieve(shipmentId);
-
-
-    let i = 0;
-
-    const indexOfUnusedRates = [];
-    shipment.rates.forEach((rate) => {
-      if (rate.service !== 'Priority') {
-        indexOfUnusedRates.push(i);
+    let priorityRate;
+    for (let i = 0; i < shipment.rates.length; i += 1) {
+      if (shipment.rates[i].service === shippingType) {
+        priorityRate = shipment.rates[i];
+        break;
       }
-      i += 1;
-    });
-
-    // shipment.rates.splice(indexOfUnusedRates, 1);
-
-    i = 0;
-    indexOfUnusedRates.forEach((index) => {
-      if (i === 0) {
-        shipment.rates.splice(index, 1);
-      } else {
-        shipment.rates.splice(index - 1, 1);
-      }
-      i += 1;
-    });
-
-
-    const { rates: { 0: shippingPrice } } = shipment;
-
-    _.set(ctx, 'body', shippingPrice);
+    }
+    // shipment.rates = [priorityRate];
+    _.set(ctx, 'body', priorityRate);
   } catch (err) {
     console.log(JSON.stringify(err));
     const message = err.Description;
