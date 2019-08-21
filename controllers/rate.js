c9099c18d52b329f871cbcc7dd861d01cbab4a90
Lilit <lil.matshkalyan@gmail.com>
2019-08-21 18:43:20 +0400

    only less that 1 pound items should be first class, otherwise it should be priority

--- controllers/rate.js
+++ controllers/rate.js
@@ -8,6 +8,7 @@ async function rateShipping(ctx) {
   const api = new Easypost(USPS_KEY);
 
   const data = _.get(ctx, 'request.body');
+  let rateService = 'Priority';
 
   let weight;
   let length;
@@ -113,9 +114,16 @@ async function rateShipping(ctx) {
     await parcel.save();
     await shipment.save();
 
+    for (let i = 0; i < shipment.rates.length; i++) { // eslint-disable-line
+      if (shipment.rates[i].service === 'First') {
+        rateService = 'First';
+        break;
+      }
+    }
+
     let priorityRate;
-    for (let i = 0; i < shipment.rates.length; i++) {
-      if (shipment.rates[i].service === 'Priority') {
+    for (let i = 0; i < shipment.rates.length; i++) { // eslint-disable-line
+      if (shipment.rates[i].service === rateService) {
         priorityRate = [shipment.rates[i]];
         break;
       }
